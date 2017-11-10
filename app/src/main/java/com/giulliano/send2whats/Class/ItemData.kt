d5826fd219b8cddcd6c8589379d0c3470a73fc71
Giulliano G. Pivetta <giullianogp@hotmail.com>
2017-11-10 13:53:19 -0200

    Melhorias Internas

--- app/src/main/java/com/giulliano/send2whats/Class/ItemData.kt
+++ app/src/main/java/com/giulliano/send2whats/Class/ItemData.kt
@@ -3,7 +3,7 @@ package com.giulliano.send2whats.Class
 /**
  * Created by Giulliano on 31/10/2017.
  */
-class ItemData(text : String, code: Int ,image: Int) {
+class ItemData(text : String, code: String ,image: Int) {
     var text = text
     var imageId = image
     var code = code
--- app/src/main/java/com/giulliano/send2whats/MainActivity.kt
+++ app/src/main/java/com/giulliano/send2whats/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.giulliano.send2whats
-
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.Intent
-import android.net.Uri
+import android.view.View
 import android.widget.AdapterView
-import android.widget.TextView
 import android.widget.Spinner
+import android.widget.TextView
 import com.giulliano.send2whats.Adapters.SpinnerAdapter
 import com.giulliano.send2whats.Class.ItemData
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var url: String = "https://api.whatsapp.com/send?phone="
+    private var lista: ArrayList<ItemData> = generateCountryList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,33 +24,50 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val sp = findViewById<Spinner>(R.id.spinner_country)
-        val adapter = SpinnerAdapter(this, R.layout.spinner_layout, R.id.txt, generateCountryList())
+        val adapter = SpinnerAdapter(this, R.layout.spinner_layout, R.id.txt, lista)
         sp.adapter = adapter
+
+
+        sp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View, position: Int, l: Long) {
+                var valor = lista[position]
+                val code = findViewById<TextView>(R.id.txt_country)
+                code.text = valor.code
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {
+                return
+            }
+        }
 
         fab.setOnClickListener { view ->
             val number = findViewById<TextView>(R.id.text_Number)
+            val code = findViewById<TextView>(R.id.txt_country)
 
-            if (number.text.isEmpty())
-            {
-                Snackbar.make(view, "Informe o número", Snackbar.LENGTH_LONG)
+            when {
+                code.text.isEmpty() -> Snackbar.make(view, R.string.codeError, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show()
-            }
-            else
-            {
-                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url + number.text))
-                startActivity(browserIntent)
+                number.text.isEmpty() -> Snackbar.make(view, R.string.numberError, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+                else -> {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url + code.text + number.text))
+                    startActivity(browserIntent)
+                }
             }
         }
+
+        val txtNumber = findViewById<TextView>(R.id.text_Number)
+        txtNumber.requestFocus()
     }
 
     private fun generateCountryList() : ArrayList<ItemData>
     {
         val list = ArrayList<ItemData>()
-        list.add(ItemData("BR", 55, R.mipmap.br))
-        list.add(ItemData("US", 1, R.mipmap.us))
-        list.add(ItemData("UK", 44, R.mipmap.uk))
-        list.add(ItemData("AR", 54, R.mipmap.ar))
-        list.add(ItemData("UY", 598, R.mipmap.uy))
+        list.add(ItemData("BR", "55", R.mipmap.br))
+        list.add(ItemData("US", "1", R.mipmap.us))
+        list.add(ItemData("UK", "44", R.mipmap.uk))
+        list.add(ItemData("AR", "54", R.mipmap.ar))
+        list.add(ItemData("UY", "598", R.mipmap.uy))
 
         return list
     }
